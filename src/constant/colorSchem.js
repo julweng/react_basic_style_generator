f7d3712867d0194d83213a09c4fddbd83e23d66f
julweng <julweng@gmail.com>
2019-06-28 16:20:17 -0700

    color scheme

---
+++ src/constant/colorSchem.js
@@ -0,0 +1,43 @@
+const colorScheme = [
+  {
+    fontColor: "#72A9CB",
+    backgroundColor: "#1675AA"
+  },
+  {
+    fontColor: "#1675AA",
+    backgroundColor: "#72A9CB"
+  },
+  {
+    fontColor: "#CBF3F0",
+    backgroundColor: "#2EC4B6"
+  },
+  {
+    fontColor: "#2EC4B6",
+    backgroundColor: "#CBF3F0"
+  },
+  {
+    fontColor: "#FF686B",
+    backgroundColor: "#FFa69E"
+  },
+  {
+    fontColor: "#FFa69E",
+    backgroundColor: "#FF686B"
+  },
+  {
+    fontColor: "#FF9F1C",
+    backgroundColor: "#FFBF69"
+  },
+  {
+    fontColor: "#FFBF69",
+    backgroundColor: "#FF9F1C"
+  },
+  {
+    fontColor: "#5E548E",
+    backgroundColor: "#E0B1CB"
+  },
+  {
+    fontColor: "#E0B1CB",
+    backgroundColor: "#5E548E"
+  }
+]
+export default colorScheme
